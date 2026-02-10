24bdcbbdddd520355054ca6fa4240e852fadb2bf
gutermanjunior <gutermanjunior@gmail.com>
2026-02-10 07:20:59 -0300

    [Windows] excludeSwitches
    Silenciar warnings do Windows

--- MonitorHU.py
+++ MonitorHU.py
@@ -64,7 +64,12 @@ class MonitorHU:
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         options.add_argument(f'user-agent={user_agent}')
         options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        
+        # --- NOVO: SILENCIAR ERROS DO CHROME ---
+        options.add_argument("--log-level=3")  # Só mostra erros fatais
+        options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"]) # Remove aviso de automação E logs de DevTools
+        # ---------------------------------------
+        
         options.add_experimental_option('useAutomationExtension', False)
         
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
